2f0ff100a965aa2c7dcec853f257ccb570013325
Peizhi Zheng <pzheng16@asu.edu>
2022-01-17 16:36:31 -0800

    init go-programming-book

---
+++ ch1/hello-world.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+funcs main() {
+	fmt.Println("hello world")
+}
---
+++ ch1/variables.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main(){
+	var a = "initial"
+	fmt.Println(a)
+	
+	var b, c int = 1, 2
+	fmt.Println(b,c)
+	
+	var d = true 
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "apple"
+	fmt.Println(f)
+	
+	var g bool 
+	fmt.Print(g)
+}
+// variables are explicitly declared and used by the compiler to e.g. check type-correctness of function calls.
